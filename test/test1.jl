c69dff3469dca56ae851d04f497f4001a24404d8
Benoît Legat <benoit.legat@gmail.com>
2018-07-25 16:23:30 +0200

    MPB -> MOI

--- test/test1.jl
+++ test/test1.jl
@@ -1,26 +1,28 @@
-function test0(solver)
+function test0(optimizer)
     @testset "basic test" begin
-        m_slave = ModelWithParams(solver = solver)
+        MOI.empty!(optimizer)
+        m_slave = ModelWithParams(optimizer = optimizer)
 
         x = Parameters(m_slave, 4.0*ones(2))
         @variable(m_slave, y[1:6])
 
         @constraint(m_slave, ctr1, 3*y[1] >= 2 - 7*x[1])
 
-        @objective(m_slave, :Min, 5*y[1])
+        @objective(m_slave, Min, 5*y[1])
 
-        solve(m_slave)
+        JuMP.optimize(m_slave)
 
-        @test 5/3 ≈ JuMP.getdual(ctr1) atol=1e-3
-        @test [35/3, 0.0] ≈ JuMP.getdual(x) atol=1e-3
-        @test [-26/3, 0.0, 0.0, 0.0, 0.0, 0.0] ≈ JuMP.getvalue(y) atol=1e-3
-        @test -130/3 ≈ JuMP.getobjectivevalue(m_slave) atol=1e-3
+        @test 5/3 ≈ JuMP.resultdual(ctr1) atol=1e-3
+        @test [35/3, 0.0] ≈ JuMP.resultdual.(x) atol=1e-3
+        @test [-26/3, 0.0, 0.0, 0.0, 0.0, 0.0] ≈ JuMP.resultvalue.(y) atol=1e-3
+        @test -130/3 ≈ JuMP.objectivevalue(m_slave) atol=1e-3
     end
 end
 
-function test1(solver)
+function test1(optimizer)
     @testset "multiple parameters" begin
-        m_slave = ModelWithParams(solver = solver)
+        MOI.empty!(optimizer)
+        m_slave = ModelWithParams(optimizer = optimizer)
 
         x = Parameters(m_slave, 4.0*ones(6))
         @variable(m_slave, y[1:6])
@@ -34,15 +36,15 @@ function test1(solver)
         @constraint(m_slave, ctr7, sum(3*y[i]+x[i] for i in 2:4) >= 2 - 7*x[3])
         @constraint(m_slave, ctr8, sum(3*y[i]+7.0*x[i]-x[i] for i in 2:4) >= 2 - 7*x[3])
 
-        @objective(m_slave, :Min, 5*y[1])
+        @objective(m_slave, Min, 5*y[1])
 
-        solve(m_slave)
+        JuMP.optimize(m_slave)
 
-        @test 5/3 ≈ getdual(ctr1) + getdual(ctr2) + getdual(ctr3) + getdual(ctr4) + getdual(ctr5) + getdual(ctr6) atol=1e-3
-        @test 0.0 ≈ getdual(ctr7) atol=1e-3
-        @test 0.0 ≈ getdual(ctr8) atol=1e-3
-        @test [0.0, 0.0, 35/3, 0.0, 0.0, 0.0] ≈ JuMP.getdual(x) atol=1e-3
-        @test [-26/3, 0.0, 0.0, 0.0, 0.0, 0.0] ≈ JuMP.getvalue(y) atol=1e-3
-        @test -130/3 ≈ JuMP.getobjectivevalue(m_slave) atol=1e-3
+        @test 5/3 ≈ JuMP.resultdual(ctr1) + JuMP.resultdual(ctr2) + JuMP.resultdual(ctr3) + JuMP.resultdual(ctr4) + JuMP.resultdual(ctr5) + JuMP.resultdual(ctr6) atol=1e-3
+        @test 0.0 ≈ JuMP.resultdual(ctr7) atol=1e-3
+        @test 0.0 ≈ JuMP.resultdual(ctr8) atol=1e-3
+        @test [0.0, 0.0, 35/3, 0.0, 0.0, 0.0] ≈ JuMP.JuMP.resultdual.(x) atol=1e-3
+        @test [-26/3, 0.0, 0.0, 0.0, 0.0, 0.0] ≈ JuMP.JuMP.resultvalue.(y) atol=1e-3
+        @test -130/3 ≈ JuMP.objectivevalue(m_slave) atol=1e-3
     end
 end
--- test/test2.jl
+++ test/test2.jl
@@ -1,18 +1,19 @@
-function test2(solver)
+function test2(optimizer)
     @testset "LessThan modification" begin
-        model = ModelWithParams(solver = solver)
+        MOI.empty!(optimizer)
+        model = ModelWithParams(optimizer = optimizer)
         α = Parameter(model, 1.0)
         ParameterJuMP.setvalue!(α, -1.0)
         @variable(model, x)
         cref = @constraint(model, x ≤ α)
         @objective(model, Max, x)
-        solve(model)
-        @test getvalue(x) == -1.0
-        @test getdual(cref) == 1.0
+        JuMP.optimize(model)
+        @test JuMP.resultvalue(x) == -1.0
+        @test JuMP.resultdual(cref) == -1.0
 
         ParameterJuMP.setvalue!(α, 2.0)
-        solve(model)
-        @test getvalue(x) == 2.0
-        @test getdual(cref) == 1.0
+        JuMP.optimize(model)
+        @test JuMP.resultvalue(x) == 2.0
+        @test JuMP.resultdual(cref) == -1.0
     end
 end
